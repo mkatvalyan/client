0f4a2b8dcff3215b957aeb9cbe0f94e1169b6812
Melisa-16 <melsabeglaryan8@gmail.com>
2025-07-11 16:22:22 +0400

    Yes

---
+++ index.js
@@ -0,0 +1,7 @@
+
+const{ Server } = require('http');
+const{ Server } = require ('socket.io');
+const httpServer = createServer();
+io.on(connection,)
+const io = new Server(httpServer,{});
+httpServer.listen(3000);
